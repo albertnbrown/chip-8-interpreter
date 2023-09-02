bc93b41cfbd394b09ae16468ec4ee6dff2d4e12a
Albert Brown <albertbrown@SonOfNothing.local>
2023-09-02 03:21:28 -0700

    some basic contstructor work for the runtime

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,4 +1,19 @@
 pub mod storage;
 pub mod display;
 
-use storage::Storage;
+pub use storage::{instruction, Storage};
+use instruction::Instruction;
+
+pub struct Runtime {
+    storage: Storage,
+}
+
+impl Runtime {
+    pub fn initialize(&mut self, file_name: String) -> Runtime {
+        return Runtime {storage: Storage::initialize(file_name)};
+    }
+    // fn parse_instruction(&mut self, instruction: Instruction) -> Operation  {
+
+    // }
+}
+
