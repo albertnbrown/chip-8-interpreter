884440b1edc4ab3754fef2cd739cd76b0d7e8a64
Albert Brown <albertbrown@SonOfNothing.local>
2023-09-03 00:02:44 -0700

    modify display to actually work like a chip-8 where draws are overwrites

--- src/runtime/display/mod.rs
+++ src/runtime/display/mod.rs
@@ -4,14 +4,15 @@ use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
 
-pub const CHIP8_WIDTH: u32 = 64;
-pub const CHIP8_HEIGHT: u32 = 32;
+pub const CHIP8_WIDTH: usize = 64;
+pub const CHIP8_HEIGHT: usize = 32;
 const SCALE_FACTOR: u32 = 20;
-const SCREEN_WIDTH: u32 = CHIP8_WIDTH * SCALE_FACTOR;
-const SCREEN_HEIGHT: u32 = CHIP8_HEIGHT * SCALE_FACTOR;
+const SCREEN_WIDTH: u32 = CHIP8_WIDTH as u32 * SCALE_FACTOR;
+const SCREEN_HEIGHT: u32 = CHIP8_HEIGHT as u32 * SCALE_FACTOR;
 
 pub struct Display {
-    canvas: Canvas<Window>,
+    canvas: Canvas<Window>, // the actually drawn window
+    representation: [[bool; CHIP8_WIDTH]; CHIP8_HEIGHT] // the internal machine's represantation of above,
 }
 
 impl Display {
@@ -34,28 +35,41 @@ impl Display {
         canvas.clear();
         canvas.present();
 
-        return Display { canvas: canvas }
+        return Display { canvas: canvas, representation: [[false; CHIP8_WIDTH]; CHIP8_HEIGHT] }
     }
 
-    pub fn draw(&mut self, pixels: &[[u8; CHIP8_WIDTH as usize]; CHIP8_HEIGHT as usize]) {
-        for (y, row) in pixels.iter().enumerate() {
-            for (x, &col) in row.iter().enumerate() {
-                let x = x as u32 * SCALE_FACTOR;
-                let y = y as u32 * SCALE_FACTOR;
+    pub fn clear(&mut self) {
+        self.canvas.clear();
+        self.representation = [[false; CHIP8_WIDTH]; CHIP8_HEIGHT];
+    }
 
-                self.canvas.set_draw_color(color(col));
+    pub fn draw(&mut self, flips: [[bool; CHIP8_WIDTH]; CHIP8_HEIGHT]) -> bool {
+        let mut carry: bool = false;
+        for (y, row) in flips.iter().enumerate() {
+            for (x, &col) in row.iter().enumerate() {
+                let canvas_x = x as u32 * SCALE_FACTOR;
+                let canvas_y = y as u32 * SCALE_FACTOR;
+
+                self.representation[y][x] ^= col;
+                if (!carry && col && !self.representation[y][x]) {
+                    carry = true;
+                }
+
+                self.canvas.set_draw_color(color(self.representation[y][x]));
                 let _ = self.canvas
-                    .fill_rect(Rect::new(x as i32, y as i32, SCALE_FACTOR, SCALE_FACTOR));
+                    .fill_rect(Rect::new(canvas_x as i32, canvas_y as i32, SCALE_FACTOR, SCALE_FACTOR));
             }
         }
-        self.canvas.present();
+        self.canvas.present(); // might need to move this to its own function that fires in the loop
+
+        return carry;
     }
 }
 
-fn color(value: u8) -> pixels::Color {
-    if value == 0 {
-        pixels::Color::RGB(0, 0, 0)
-    } else {
+fn color(value: bool) -> pixels::Color {
+    if value {
         pixels::Color::RGB(0, 250, 0)
+    } else {
+        pixels::Color::RGB(0, 0, 0)
     }
 }
