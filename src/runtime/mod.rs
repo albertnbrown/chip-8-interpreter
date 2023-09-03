f84a9abef3bfcc92a965be0b5ef15c320e4ef413
Albert Brown <albertbrown@SonOfNothing.local>
2023-09-02 17:56:38 -0700

    working framework for instruction handlers added a dummy for the display for type reasons

--- src/runtime/mod.rs
+++ src/runtime/mod.rs
@@ -1,16 +1,40 @@
 pub mod storage;
 pub mod display;
+mod operators;
 
-pub use storage::{instruction, Storage};
-use instruction::Instruction;
+use storage::{instruction::Instruction, Storage};
+use operators::*;
+use display::Display;
+
+const OPCODE_INITIAL_CASES: usize = 16;
 
 pub struct Runtime {
     storage: Storage,
+    opcode_handlers: [fn(&mut Storage, &mut Display, Instruction);16],
 }
 
 impl Runtime {
     pub fn initialize(&mut self, file_name: String) -> Runtime {
-        return Runtime {storage: Storage::initialize(file_name)};
+        let opcode_handlers: [fn(&mut Storage, &mut Display, Instruction); OPCODE_INITIAL_CASES] = [
+            handle0,
+            handle1,
+            handle2,
+            handle3,
+            handle4,
+            handle5,
+            handle6,
+            handle7,
+            handle8,
+            handle9,
+            handleA,
+            handleB,
+            handleC,
+            handleD,
+            handleE,
+            handleF,
+        ];
+        let storage: Storage = Storage::initialize(file_name);
+        return Runtime {storage,  opcode_handlers};
     }
     // fn parse_instruction(&mut self, instruction: Instruction) -> Operation  {
 
---
+++ src/runtime/operators/mod.rs
@@ -0,0 +1,65 @@
+use crate::runtime::*;
+
+pub fn handle0(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+    
+}
+
+pub fn handle1(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handle2(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handle3(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handle4(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handle5(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handle6(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handle7(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handle8(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handle9(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handleA(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handleB(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handleC(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handleD(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handleE(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
+
+pub fn handleF(storage: &mut Storage, display: &mut Display, instruction: Instruction) {
+
+}
